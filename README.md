c3b286698a1af0db3558e215a664d2db4f995cae
JkYou <ups2017@163.com>
2019-09-19 17:55:47 +0800

    初始化项目呀呀呀

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sw-cli
-a cli fro swy
+前端vue/react模板初始化脚手架
+
+安装方法
+ ```
+ npm install sw-cli_swy -g
+
+ ```
+ 
---
+++ utils/generate-project.js
@@ -0,0 +1,48 @@
+const Metalsmith=require('metalsmith')
+const inquirer=require('inquirer')
+const chalk=require('chalk')
+const path=require('path')
+const ora=require('ora')
+const transformIntoAbsolutePath=require('./local-path').transformIntoAbsolutePath
+const renderTemplateFiles=require('./render-template-files')
+
+
+
+
+module.exports=(tmpPath)=>{
+  const metalsmith=Metalsmith(tmpPath)
+  inquirer.prompt([{
+    type:'input',
+    name:'name',
+    message:'The name of project',
+    default:'swy-fe'
+  },{
+    type:'input',
+    name:'destination',
+    message:'The destination of project',
+    default:process.cwd()
+  }]).then(answer=>{
+    //项目生成路径
+    const destination=path.join(transformIntoAbsolutePath(answer.destination),answer.name)
+    const spinner = ora('generating...').start()
+    //加入新的全局变量
+    Object.assign(metalsmith.metadata(),answer)
+    
+    spinner.start()
+
+    metalsmith
+    .source('.')
+    .destination(destination)
+    .clean(false)
+    .build(function(err) {      
+      spinner.stop()
+      if (err) throw err
+      console.log()
+      console.log(chalk.green('Build Successfully'))
+      console.log()
+      console.log((`${chalk.green('Please cd')} ${destination} ${chalk.green('to start your coding')}`))
+      console.log()
+    })
+  })
+}
+
